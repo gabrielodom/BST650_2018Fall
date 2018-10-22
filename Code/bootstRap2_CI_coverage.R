03e9934398deceb7335f96d1ef181b3c752a9f03
gabrielodom <gabriel_odom@baylor.edu>
2018-10-22 10:52:11 -0400

    Answers to bootstrap assignment 2

--- Code/bootstRap2_CI_coverage.R
+++ Code/bootstRap2_CI_coverage.R
@@ -47,3 +47,69 @@ mean(experimentN10_logi, na.rm = TRUE)
 # We want this number to be as close to 0.95 as possible, without going under.
 #   A few of the t-test confidence intervals fail, so we remove them with the
 #   option na.rm = TRUE.
+
+
+
+######  Repeat with CheckCoverage() Function  #################################
+CheckCoverage(x = wcgs_df$BMI, n = 10)
+
+# Repeat 10k times
+covered_logi <- replicate(10000, CheckCoverage(x = wcgs_df$BMI, n = 10))
+# Number of times we failed to reject (correct outcome):
+sum(covered_logi)
+# Number of times we rejected (incorrect outome):
+sum(!covered_logi)
+# Coverage probability:
+mean(covered_logi)
+
+# Change alpha = 0.2
+covered_logi <- replicate(
+  10000,
+  CheckCoverage(x = wcgs_df$BMI, n = 10, alpha = 0.2)
+)
+mean(covered_logi)
+
+# Change n = 100
+covered_logi <- replicate(
+  10000,
+  CheckCoverage(x = wcgs_df$BMI, n = 100)
+)
+mean(covered_logi)
+
+
+######  arcus  ################################################################
+hist(wcgs_df$arcus)
+mean(
+  replicate(
+    100000,
+    CheckCoverage(x = wcgs_df$arcus, n = 10)
+  ),
+  na.rm = TRUE
+)
+# 99% for n = 10, 95.3% for n = 30, 94.9% for n = 50
+
+
+
+######  height  ###############################################################
+hist(wcgs_df$height)
+mean(
+  replicate(
+    100000,
+    CheckCoverage(x = wcgs_df$height, n = 10)
+  ),
+  na.rm = TRUE
+)
+# 95.0% for n = 10, 95% for n = 30, 95.2% for n = 50
+
+
+
+######  ncigs  ################################################################
+hist(wcgs_df$ncigs)
+mean(
+  replicate(
+    100000,
+    CheckCoverage(x = wcgs_df$ncigs, n = 50)
+  ),
+  na.rm = TRUE
+)
+# 93.1% for n = 10, 94.3% for n = 30, 94.6% for n = 50
---
+++ Code/bootstRap2_CheckCoverage_function.R
@@ -0,0 +1,25 @@
+
+CheckCoverage <- function(x, n, alpha = 0.05){
+  
+  # Step 0: Clean the Data
+  x <- x[!is.na(x)] # Remove missing values
+  
+  # Step 1: The "Truth" (null hypothesis)
+  xMu <- mean(x, na.rm = TRUE) # "true" mean
+  
+  # Step 2: randomly select n values
+  sampX <- sample(x, size = n, replace = FALSE)
+  
+  # Step 3: Construct a 95% CI
+  t_mod <- t.test(
+    x = sampX,
+    mu = xMu,
+    alternative = "two.sided",
+    conf.level = 1 - alpha
+  )
+  
+  # Step 4: Test if the population mean is contained in the CI
+  CI <- t_mod$conf.int
+  (CI[1] < xMu) && (xMu < CI[2])
+  
+}
